273be8bd8fc609630c1e19a4915bb20d27788e45
Kelvyn <kelvynsprete@gmail.com>
2025-06-19 12:34:47 -0300

    Add merge logic

--- auto-git.sh
+++ auto-git.sh
@@ -1,15 +1,44 @@
-#!/bin/bash
+function exit_exception (){
+    if [ $? -eq 130 ]; then
+        echo "Exiting..."
+        exit 1
+    fi
+}
+
+function switch_branch () {
+
+    selected=$(git branch | fzf +m \
+        --height 40% \
+        --layout reverse \
+        --border \
+        --preview \
+            'git -c color.ui=always log --oneline $(echo {} | tr -d "* ")' \
+        --color bg:#222222,preview-bg:#333333)
+    
+    exit_exception
+
+    selected=$(echo $selected | tr -d "* ")
+
+    git switch "$selected"
+}
 
 
-selected=$(git branch | fzf \
---height 40% \
---layout  reverse \
---border \
---preview \
-    'git log --oneline')
+function merge () {
 
-selected=$(echo $selected | tr -d "* ")
+    selected=$(git branch | fzf +m \
+        --height 100% \
+        --layout reverse \
+        --border \
+        --preview \
+            'git -c color.ui=always diff $(git branch | grep "^* | tr -d "* ") $(echo {} | tr -d "* ")' \
+        --color bg:#222222,preview-bg:#333333)
+    
+    exit_exception
 
-echo "$selected"
+    selected=$(echo $selected | tr -d "* ")
 
-git switch "$selected"
+    git merge "$selected"
+}
+
+
+merge
