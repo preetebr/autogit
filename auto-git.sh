eabff9f0cc611e8071201cf79a5e2b57a46d6152
Kelvyn <kelvynsprete@gmail.com>
2025-06-19 12:39:13 -0300

    Add install

--- auto-git.sh
+++ auto-git.sh
@@ -1,44 +1,118 @@
+#!/bin/bash
+
 function exit_exception (){
     if [ $? -eq 130 ]; then
         echo "Exiting..."
         exit 1
-    fi
+    fi   
 }
 
-function switch_branch () {
-
+function switch_branch (){
+    
     selected=$(git branch | fzf +m \
+        --header "Select the branch to go:" \
         --height 40% \
         --layout reverse \
-        --border \
+        --border\
         --preview \
             'git -c color.ui=always log --oneline $(echo {} | tr -d "* ")' \
         --color bg:#222222,preview-bg:#333333)
     
-    exit_exception
-
+    exit_exception 
+    
     selected=$(echo $selected | tr -d "* ")
-
+    
     git switch "$selected"
 }
 
-
-function merge () {
+function merge (){
 
     selected=$(git branch | fzf +m \
+        --header "Select the branch to merge:" \
         --height 100% \
         --layout reverse \
-        --border \
+        --border\
         --preview \
-            'git -c color.ui=always diff $(git branch | grep "^* | tr -d "* ") $(echo {} | tr -d "* ")' \
+            'git -c color.ui=always diff $(git branch | grep "^*" | tr -d "* " ) $(echo {} | tr -d "* ")' \
         --color bg:#222222,preview-bg:#333333)
     
-    exit_exception
-
+    exit_exception 
+    
     selected=$(echo $selected | tr -d "* ")
-
+    
     git merge "$selected"
 }
 
+function delete_branch (){
+    
+    selected=$(git branch | fzf +m \
+        --header "select the branch to delete:" \
+        --height 40% \
+        --layout reverse \
+        --border\
+        --preview \
+            'git -c color.ui=always log --oneline $(echo {} | tr -d "* ")' \
+        --color bg:#222222,preview-bg:#333333)
+    
+    exit_exception 
+    
+    selected=$(echo $selected | tr -d "* ")
+    
+    git branch -d "$selected"
+}
 
-merge
+function main (){
+    
+    options=(\
+        "1 - Switch branch" \
+        "2 - Git merge" \
+        "3 - Delete branch" \
+        "Exit"\
+        )
+
+    selected=$( for opt in "${options[@]}" ; do echo $opt ; done | fzf +m \
+        --header "Select one option:" \
+        --height 40% \
+        --layout reverse \
+        --border\
+        --color bg:#222222)
+
+    exit_exception
+    
+    case "$selected" in
+        ${options[0]})
+            echo "$selected"
+            switch_branch
+            exit 0
+        ;;
+        ${options[1]})
+            echo "$selected"
+            merge
+            exit 0
+        ;;
+        ${options[2]})
+            echo "$selected"
+            delete_branch
+            exit 0
+        ;;
+        ${options[3]})
+            echo "$selected"
+            exit 0
+        ;;
+        *)
+        exit 0
+    esac
+}
+
+function validate_git_repository (){
+
+    git status >> /dev/null 2>&1
+    
+    if [ $? -eq 128 ]; then
+        echo "This not is a git repository!"
+        exit 1
+    fi
+}
+
+validate_git_repository 
+main
---
+++ install.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+LOCAL_DIR="$HOME/.local"
+BIN_DIR="$LOCAL_DIR/bin"
+SCRIPT_DIR="$LOCAL_DIR/auto-git"
+SCRIPT_FILE="$SCRIPT_DIR/auto-git.sh"
+TARGET_SCRIPT="$BIN_DIR/auto-git"
+ALIAS_NAME="ag"
+ALIAS_CMD="alias $ALIAS_NAME=\"$TARGET_SCRIPT\""
+BASHRC="$HOME/.bashrc"
+
+function log_message() {
+    echo "$(date '+%d-%m-%Y %H:%M:%S') - $1"
+}
+
+function validate_local_dir () {
+    if [ ! -d "$LOCAL_DIR" ]; then 
+        log_message "Creating $LOCAL_DIR directory."
+        mkdir "$LOCAL_DIR"
+    fi
+
+    if [ ! -d "$BIN_DIR" ]; then
+        log_message "Creating $BIN_DIR directory."
+        mkdir "$BIN_DIR"
+    fi
+}
+
+function update_bashrc () {
+    if ! grep -q "$ALIAS_CMD" "$BASHRC"; then
+        echo "$ALIAS_CMD" >> "$BASHRC"
+        log_message "Alias '$ALIAS_NAME' added to $BASHRC."
+    else
+        log_message "An alias named '$ALIAS_NAME' already exists. If it is not for auto-git, please configure it manually."
+    fi
+}
+
+function install () {
+    validate_local_dir
+
+    if [ ! -f "$SCRIPT_FILE" ]; then
+        log_message "Script $SCRIPT_FILE not found. Installation aborted."
+        return 1
+    fi
+
+    cp "$SCRIPT_FILE" "$TARGET_SCRIPT"
+    chmod +x "$TARGET_SCRIPT"
+    log_message "Script auto-git installed in $TARGET_SCRIPT."
+
+    update_bashrc
+
+    rm -rf "$SCRIPT_DIR"
+    log_message "Directory $SCRIPT_DIR deleted."
+
+    log_message "Installation completed successfully!"
+    log_message "***Restart the terminal to everything run corretly***"
+}
+
+function main () {
+    if [ -f "$TARGET_SCRIPT" ]; then
+        log_message "Upgrading auto-git..."
+    else
+        log_message "Installing auto-git..."
+    fi
+    install
+}
+
+main
